c5e9c18b7cadd96de949804982f7541fedad771b
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-01-23 20:57:31 +0800

    jest test for memo resolver and service

--- src/memo/memo.resolver.spec.ts
+++ src/memo/memo.resolver.spec.ts
@@ -1,19 +1,179 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Neo4jModule } from 'nest-neo4j';
+import { PersonModule } from '../person/person.module';
+import { CreateMemoInput, LinkPersonInput, ListMemoInput } from './dto/memo.dto';
+import { Memo, MemoInfo, MemoTitle } from './entities/memo.entity';
 import { MemoResolver } from './memo.resolver';
 import { MemoService } from './memo.service';
 
+jest.mock('neo4j-driver/lib/driver')
+
 describe('MemoResolver', () => {
-  let resolver: MemoResolver;
+  let memoResolver: MemoResolver;
+  let memoService: MemoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        Neo4jModule.forRoot({ //some dummmy config 
+          scheme: 'neo4j',
+          host: 'localhost',
+          port: 7687,
+          username: 'neo4j',
+          password: 'graph'
+        }),
+        PersonModule
+      ],
+
       providers: [MemoResolver, MemoService],
     }).compile();
 
-    resolver = module.get<MemoResolver>(MemoResolver);
+    memoResolver = module.get<MemoResolver>(MemoResolver);
+    memoService = module.get<MemoService>(MemoService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  describe('createMemo', () => {
+    it('should return the memo just created', async () => {
+      const createMemoInput: CreateMemoInput = {
+        title: 'Title 1',
+        content: 'Content 1',
+        personId: '1'
+      }
+
+      const memo: MemoInfo = {
+        ...createMemoInput,
+        id: '0',
+        creationDate: new Date(),
+        updateDate: new Date()
+      }
+
+      jest.spyOn(memoService, 'create').mockResolvedValue({
+        success: true,
+        data: memo
+      })
+
+      return expect(await memoResolver.createMemo(createMemoInput)).toBe(memo)
+    });
+
+    it('should throw exception if service return not success to create memo', async () => {
+      const createMemoInput: CreateMemoInput = {
+        title: 'Title 1',
+        content: 'Content 1',
+        personId: '1'
+      }
+
+      jest.spyOn(memoService, 'create').mockResolvedValue({
+        success: false,
+        errorMessage: 'Some errors'
+      })
+
+      try {
+        await memoResolver.createMemo(createMemoInput)
+      } catch (e) {
+        expect(e.response).toMatch('Some errors')
+      }
+    })
+
+  });
+
+  describe('findAll', () => {
+    it('should return all id and title of memos belongs to the person', async () => {
+      const listMemoInput: ListMemoInput = {
+        personId: '0'
+      }
+
+      const memos = [{
+        id: '0',
+        title: 'Title 1',
+        content: 'Content 1',
+        creationDate: new Date(),
+        updateDate: new Date()
+      }, {
+        id: '1',
+        title: 'Title 2',
+        content: 'Content 2',
+        creationDate: new Date(),
+        updateDate: new Date()
+      }]
+
+      jest.spyOn(memoService, 'findByPersonId').mockResolvedValue(memos)
+
+      const res = await memoResolver.findAll(listMemoInput)
+      expect(res).toBe(memos)
+    });
+  });
+
+  describe('linkPerson', () => {
+    it('should return the linkId from the relationship just created', async () => {
+      const linkPersonInput: LinkPersonInput = {
+        memoId: '0',
+        personId: '0'
+      }
+
+      jest.spyOn(memoService, 'linkPerson').mockResolvedValue({
+        success: true,
+        data: {
+          linkId: '0'
+        }
+      })
+
+      const res = await memoResolver.linkPerson(linkPersonInput)
+      expect(res).toHaveProperty('linkId', '0')
+    });
+
+    it('should throw exception if service return not success to link person', async () => {
+      const linkPersonInput: LinkPersonInput = {
+        memoId: '0',
+        personId: '0'
+      }
+
+      jest.spyOn(memoService, 'linkPerson').mockResolvedValue({
+        success: false,
+        errorMessage: 'Some Errors'
+      })
+
+      try {
+        await memoResolver.linkPerson(linkPersonInput)
+      } catch (e) {
+        expect(e.response).toMatch('Some Errors')
+      }
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return memo and its linked persons', async () => {
+      const id = '0';
+
+      const result: Memo = {
+        id: '0',
+        title: 'Title 1',
+        content: 'Content 1',
+        creationDate: new Date(),
+        updateDate: new Date(),
+        personId: '0',
+        persons: [{
+          id: '0',
+          displayName: 'Person 1',
+          creationDate: new Date(),
+          updateDate: new Date()
+        }]
+      }
+
+      jest.spyOn(memoService, 'findOne').mockResolvedValue(result)
+
+      const res = await memoResolver.findOne(id)
+      expect(res).toBe(result)
+    });
+
+    it('should return null if not found', async () => {
+      const id = '0';
+
+      const result = null;
+
+      jest.spyOn(memoService, 'findOne').mockResolvedValue(result)
+
+      const res = await memoResolver.findOne(id)
+      expect(res).toBe(result)
+    });
   });
 });
--- src/memo/memo.service.spec.ts
+++ src/memo/memo.service.spec.ts
@@ -1,18 +1,229 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Neo4jModule, Neo4jService } from 'nest-neo4j';
+import { PersonModule } from '../person/person.module';
 import { MemoService } from './memo.service';
+import { mockResult, mockNode, } from 'nest-neo4j/dist/test';
+import { DateTime } from 'neo4j-driver/lib/temporal-types.js';
+import { LinkPersonInput, ListMemoInput } from './dto/memo.dto';
+import { Memo } from './entities/memo.entity';
 
 describe('MemoService', () => {
-  let service: MemoService;
+  let memoService: MemoService;
+  let neo4jService: Neo4jService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        Neo4jModule.forRoot({ //some dummmy config 
+          scheme: 'neo4j',
+          host: 'localhost',
+          port: 7687,
+          username: 'neo4j',
+          password: 'graph'
+        }),
+        PersonModule
+      ],
       providers: [MemoService],
     }).compile();
 
-    service = module.get<MemoService>(MemoService);
+    memoService = module.get<MemoService>(MemoService);
+    neo4jService = module.get<Neo4jService>(Neo4jService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    const data = {
+      title: 'Title 1',
+      content: 'Content 1',
+      personId: '1',
+      creationDate: new Date(),
+      updateDate: new Date()
+    }
+
+    it('return a success result with memo data if create successfully', async () => {
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult([
+            {
+              count: 1
+            },
+          ])
+        )
+
+      jest.spyOn(neo4jService, 'write')
+        .mockResolvedValue(
+          mockResult([
+            {
+              memo: mockNode('Memo', {
+                ...data,
+                creationDate: DateTime.fromStandardDate(data.creationDate),
+                updateDate: DateTime.fromStandardDate(data.updateDate)
+              })
+            },
+          ])
+        )
+      const res = await memoService.create(data)
+      expect(res.success).toEqual(true)
+      expect(res.data).toHaveProperty('id')
+      expect(res.data).toHaveProperty('title', data.title)
+      expect(res.data).toHaveProperty('content', data.content)
+      expect(res.data).toHaveProperty('personId', data.personId)
+    });
+
+    it('return a fail result with errorMessage if owner not exist', async () => {
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult([
+            {
+              count: 0
+            },
+          ])
+        )
+
+      const res = await memoService.create(data)
+      expect(res.success).toEqual(false)
+      expect(res.errorMessage).toBe(`Owner #${data.personId} does not exist`)
+    });
+  });
+
+  describe('findByPersonId', () => {
+    const listMemoInput: ListMemoInput = {
+      personId: '1'
+    }
+
+    const memoData = [{
+      title: 'Title 1',
+      content: 'Content 1',
+      personId: '1',
+      creationDate: new Date(),
+      updateDate: new Date()
+    }]
+
+    it('return an array of memos', async () => {
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult(
+            memoData.map(data => {
+              return {
+                memo: mockNode('Memo', {
+                  ...data,
+                  creationDate: DateTime.fromStandardDate(data.creationDate),
+                  updateDate: DateTime.fromStandardDate(data.updateDate)
+                })
+              }
+            })
+          )
+        )
+
+
+      const res = await memoService.findByPersonId(listMemoInput)
+      expect(res).toHaveLength(1)
+      expect(res[0]).toHaveProperty('title', memoData[0].title)
+      expect(res[0]).toHaveProperty('content', memoData[0].content)
+      expect(res[0]).toHaveProperty('personId', memoData[0].personId)
+      expect(res[0]).toHaveProperty('creationDate', memoData[0].creationDate)
+      expect(res[0]).toHaveProperty('updateDate', memoData[0].updateDate)
+    });
+  });
+
+  describe('linkPerson', () => {
+    const linkPersonInput: LinkPersonInput = {
+      memoId: '1',
+      personId: '1'
+    }
+
+
+    it('return a success result with link id if link memo and person successfully', async () => {
+
+      jest.spyOn(neo4jService, 'write')
+        .mockResolvedValue(
+          mockResult([
+            {
+              r: mockNode('LINKED_TO', {})
+            },
+          ])
+        )
+      const res = await memoService.linkPerson(linkPersonInput)
+      expect(res.success).toEqual(true)
+      expect(res).toHaveProperty('data')
+      expect(res.data).toHaveProperty('linkId')
+    });
+
+    it('return a fail result errorMessage if the person is owner', async () => {
+
+      jest.spyOn(neo4jService, 'write')
+        .mockResolvedValue(
+          mockResult([])
+        )
+      const res = await memoService.linkPerson(linkPersonInput)
+      expect(res.success).toEqual(false)
+      expect(res).toHaveProperty('errorMessage', 'Cannot link memo to its owner')
+    });
+  });
+
+  describe('findOne', () => {
+
+    const id = '0';
+    const data = {
+      title: 'Title 1',
+      content: 'Content 1',
+      creationDate: new Date(),
+      updateDate: new Date(),
+      personId: '0',
+      persons: [{
+        displayName: 'Person 1',
+        creationDate: new Date(),
+        updateDate: new Date()
+      }]
+    }
+
+
+    it('return memo and its linked persons', async () => {
+
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult([
+            {
+              memo: mockNode('Memo', {
+                ...data,
+                creationDate: DateTime.fromStandardDate(data.creationDate),
+                updateDate: DateTime.fromStandardDate(data.updateDate)
+              }),
+              persons: data.persons.map(p => {
+                const pObj = mockNode('Person', {
+                  ...p,
+                  creationDate: DateTime.fromStandardDate(p.creationDate),
+                  updateDate: DateTime.fromStandardDate(p.updateDate),
+                })
+                return pObj
+
+              })
+            }
+          ])
+        )
+      const res = await memoService.findOne(id)
+
+      expect(res).toHaveProperty('id')
+      expect(res).toHaveProperty('title', 'Title 1')
+      expect(res).toHaveProperty('content', 'Content 1')
+      expect(res).toHaveProperty('creationDate', data.creationDate)
+      expect(res).toHaveProperty('updateDate', data.updateDate)
+      expect(res).toHaveProperty('persons')
+      expect(res.persons).toHaveLength(1)
+      expect(res.persons[0]).toHaveProperty('id')
+      expect(res.persons[0]).toHaveProperty('displayName', 'Person 1')
+    });
+
+    it('return null if no memo found', async () => {
+
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult([])
+        )
+      const res = await memoService.findOne(id)
+
+      expect(res).toBeNull()
+    });
+
+
   });
 });
