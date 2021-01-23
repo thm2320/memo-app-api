fd2e404f8e4d9da058342906619bebcd2afe56fb
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-01-23 16:35:35 +0800

    jest test for person resolver and service

--- src/person/person.resolver.spec.ts
+++ src/person/person.resolver.spec.ts
@@ -1,19 +1,101 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Neo4jModule } from 'nest-neo4j';
+import { CreatePersonInput } from './dto/person.dto';
+import { Person } from './entities/person.entity';
 import { PersonResolver } from './person.resolver';
 import { PersonService } from './person.service';
 
+jest.mock('neo4j-driver/lib/driver')
+
 describe('PersonResolver', () => {
-  let resolver: PersonResolver;
+  let personResolver: PersonResolver;
+  let personService: PersonService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        Neo4jModule.forRoot({ //some dummmy config 
+          scheme: 'neo4j',
+          host: 'localhost',
+          port: 7687,
+          username: 'neo4j',
+          password: 'graph'
+        })
+      ],
       providers: [PersonResolver, PersonService],
     }).compile();
 
-    resolver = module.get<PersonResolver>(PersonResolver);
+    personResolver = module.get<PersonResolver>(PersonResolver);
+    personService = module.get<PersonService>(PersonService);
+
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+  describe('findAll', () => {
+    it('should return an array of persons', () => {
+      const persons: Person[] = [{
+        id: '0',
+        displayName: 'Person 1',
+        creationDate: new Date(),
+        updateDate: new Date()
+      }, {
+        id: '1',
+        displayName: 'Person 2',
+        creationDate: new Date(),
+        updateDate: new Date()
+      }]
+
+      jest.spyOn(personService, 'findAll').mockResolvedValue(persons)
+
+      return expect(personResolver.findAll()).resolves.toBe(persons)
+    });
+  })
+
+  describe('createPerson', () => {
+    it('should return the person just created', async () => {
+      const createPersonInput: CreatePersonInput = {
+        displayName: 'Person 1'
+      }
+
+      const person: Person = {
+        id: '0',
+        displayName: createPersonInput.displayName,
+        creationDate: new Date(),
+        updateDate: new Date()
+      }
+
+      jest.spyOn(personService, 'create').mockResolvedValue({
+        success: true,
+        data: person
+      })
+
+      return expect(await personResolver.createPerson(createPersonInput)).toBe(person)
+    });
+
+    it('should throw exception if service return not success to create person', async () => {
+      const createPersonInput: CreatePersonInput = {
+        displayName: 'Person 1'
+      }
+
+      const person: Person = {
+        id: '0',
+        displayName: createPersonInput.displayName,
+        creationDate: new Date(),
+        updateDate: new Date()
+      }
+
+      jest.spyOn(personService, 'create').mockResolvedValue({
+        success: false,
+        errorMessage: 'Some errors'
+      })
+
+      try {
+        await personResolver.createPerson(createPersonInput)
+      } catch (e) {
+        expect(e.response).toMatch('Some errors')
+      }
+
+    });
+
+  })
+
 });
--- src/person/person.service.spec.ts
+++ src/person/person.service.spec.ts
@@ -1,18 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PersonService } from './person.service';
+import { Neo4jModule, Neo4jService } from 'nest-neo4j';
+import { mockResult, mockNode } from 'nest-neo4j/dist/test';
+import { DateTime } from 'neo4j-driver/lib/temporal-types.js';
+
+jest.mock('neo4j-driver/lib/driver')
 
 describe('PersonService', () => {
-  let service: PersonService;
+  let personService: PersonService;
+  let neo4jService: Neo4jService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        Neo4jModule.forRoot({ //some dummmy config 
+          scheme: 'neo4j',
+          host: 'localhost',
+          port: 7687,
+          username: 'neo4j',
+          password: 'graph'
+        })
+      ],
       providers: [PersonService],
     }).compile();
 
-    service = module.get<PersonService>(PersonService);
+    personService = module.get<PersonService>(PersonService);
+    neo4jService = module.get<Neo4jService>(Neo4jService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('create', () => {
+    const data = {
+      displayName: 'Person 1',
+      creationDate: new Date(),
+      updateDate: new Date()
+    }
+
+    it('return a success result with person data if create successfully', async () => {
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult([
+            {
+              count: 0
+            },
+          ])
+        )
+
+      jest.spyOn(neo4jService, 'write')
+        .mockResolvedValue(
+          mockResult([
+            {
+              person: mockNode('Person', {
+                ...data,
+                creationDate: DateTime.fromStandardDate(data.creationDate),
+                updateDate: DateTime.fromStandardDate(data.updateDate)
+              })
+            },
+          ])
+        )
+      const res = await personService.create(data)
+      expect(res.success).toEqual(true)
+      expect(res.data).toHaveProperty('id')
+      expect(res.data).toHaveProperty('displayName', data.displayName)
+      expect(res.data).toHaveProperty('creationDate', data.creationDate)
+      expect(res.data).toHaveProperty('updateDate', data.updateDate)
+    });
+
+    it('return a fail result with errorMessage if displayName already exists', async () => {
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult([
+            {
+              count: 1
+            },
+          ])
+        )
+
+      const res = await personService.create(data)
+      expect(res.success).toEqual(false)
+      expect(res).toHaveProperty('errorMessage', `Person "${data.displayName}" already exists`)
+
+    });
+  })
+
+  describe('findAll', () => {
+    const personData = [{
+      displayName: 'Person 1',
+      creationDate: new Date(),
+      updateDate: new Date()
+    }, {
+      displayName: 'Person 2',
+      creationDate: new Date(),
+      updateDate: new Date()
+    }]
+
+    it('return an array of persons', async () => {
+
+
+      jest.spyOn(neo4jService, 'read')
+        .mockResolvedValue(
+          mockResult(
+            personData.map(data => {
+              return {
+                n: mockNode('Person', {
+                  ...data,
+                  creationDate: DateTime.fromStandardDate(data.creationDate),
+                  updateDate: DateTime.fromStandardDate(data.updateDate)
+                })
+              }
+            })
+          )
+        )
+      const res = await personService.findAll();
+
+      expect(res).toHaveLength(2)
+      expect(res[0]).toHaveProperty('displayName', personData[0].displayName)
+      expect(res[0]).toHaveProperty('creationDate', personData[0].creationDate)
+      expect(res[0]).toHaveProperty('updateDate', personData[0].updateDate)
+    })
+
+
+  })
 });
